26b4ab78564b246fa9a45e5198f2d8e21c562fea
titanfall <qkrwogh7366@gmail.com>
2025-03-09 13:40:06 +0900

    3-4

---
+++ .vscode/program3_4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#define MAX_ROWS 3
+#define MAX_COLS 5
+int matrix[MAX_ROWS][MAX_COLS]; // 2차원 배열 
+//희소행렬의 전치행렬 구현 방법1 
+
+void matrix_transpose(int A[MAX_ROWS][MAX_COLS],int B[MAX_ROWS][MAX_COLS])
+{
+    for(int r=0;r<MAX_ROWS;r++){
+        for(int c=0;c<MAX_COLS;c++){
+            B[r][c]=A[c][r]; // 행과 열을 ij에서 ji로 변환
+        }
+    }
+}
+void matrix_print(int A[MAX_ROWS][MAX_COLS]){
+    printf("==============================");
+    for(int r=0;r<MAX_ROWS;r++){
+        for(int c=0;c<MAX_COLS;c++){
+            printf("%d",A[r][c]);
+        }
+    }
+    printf("===============================");
+}
+int main(void)
+{
+    int array1[MAX_ROWS][MAX_COLS]={{2,3,0},
+                                    {8,9,1},
+                                    {7,0,5}};
+    int array2[MAX_ROWS][MAX_COLS];
+
+    matrix_transpose(array1,array2); // 전치행렬 변환
+    matrix_print(array1);
+    matrix_print(array2);
+
+    return 0;
+}
