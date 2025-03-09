a55273fd70fbfeb469d1b4d7cd50986412b8802c
titanfall <qkrwogh7366@gmail.com>
2025-03-09 16:16:52 +0900

    3-7

---
+++ .vscode/exercise 3_7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//복소수를 구조체로 표현하여보자. 복소수 a와 복소수 b를 받아서 a+b를 계산하는 함수를 작성해보자. 함수는 구조체를 반환할 수 있다. 알다시피 복소수는 real+ imag*i의 형태를 갖는다.
+typedef struct Complex{
+    float real;
+    float imag;
+}Complex;
+
+Complex complex_add(Complex a,Complex b){
+
+}
+
+void print_Complex(m){
+    printf("--------------------------------\n");
+    printf("%f %f*i \n",m.real,m.imag); // float 함수는 printf 함수에서 자동으로 double로 승격된다. 따라서 lf와 f 둘다 써도 상관이 없다.
+    printf("--------------------------------\n");
+}
+int main(void){
+    Complex A={2,5};
+    Complex B={4,3};
+    Complex C;
+
+    C=complex_add(A,B);
+
+    print_Complex(A);
+    print_complex(B);
+
+}
